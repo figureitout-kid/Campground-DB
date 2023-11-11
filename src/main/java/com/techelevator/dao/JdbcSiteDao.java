f9b56c793759d89e7905e95485eb284e28e9c707
Alex <a12.schreck@gmail.com>
2023-11-10 20:50:03 -0500

    FINISHED

--- src/main/java/com/techelevator/dao/JdbcSiteDao.java
+++ src/main/java/com/techelevator/dao/JdbcSiteDao.java
@@ -40,13 +40,32 @@ public class JdbcSiteDao implements SiteDao {
         }
         return sites;
     }
+//    CURRENT_DATE + 1, CURRENT_DATE + 2, CURRENT_DATE - 10
+//    from_date has to be AFTER from_date >  CURRENT_DATE
+//    to_date
+//        NULL
+    // if current_date is after from date - no
+    // if current_date before, yes
+        // to_date - from_date = days
+        // current_date uses days to save time searching
+    // NULL exceptions?
+//  "WHERE park_id = ? AND CURRENT_DATE NOT BETWEEN from_date AND to_date;";
+    //((from_date > CURRENT_DATE) AND (to_date < CURRENT_DATE)
+
+    //  "WHERE park_id = ? AND (((from_date >= CURRENT_DATE) OR (from_date IS NULL)) AND ((CURRENT_DATE < to_date) OR (to_date IS NULL)));";
+//   "WHERE park_id = ? AND CURRENT_DATE NOT BETWEEN from_date AND to_date;";
+//  //                 11/8             11/10
+//   "AND ((reservation.to_date < CURRENT_DATE AND reservation.from_date >= CURRENT_DATE) OR reservation.from_date IS NULL) "+
+//    "WHERE campground.park_id = ? AND reservation.reservation_id IS NULL;";
+
 
     @Override
     public List<Site> getSitesWithoutReservationByParkId(int parkId) {
         List<Site> sites = new ArrayList<>();
-        String sql = "SELECT * FROM site JOIN campground ON site.campground_id = campground.campground_id " +
-        "LEFT JOIN reservation ON site.site_id = reservation.site_id " +
-        "WHERE park_id = ? start_date = ? AND end_date = ?;";
+            String sql = "SELECT DISTINCT site.campground_id, site.site_id, site.site_number, max_occupancy, accessible, max_rv_length, utilities FROM site " +
+            "JOIN campground ON site.campground_id = campground.campground_id " +
+            "JOIN reservation ON site.site_id = reservation.site_id " +
+            "WHERE park_id = ? AND (reservation.to_date < CURRENT_DATE OR (reservation.to_date > CURRENT_DATE AND reservation.from_date > CURRENT_DATE));";
         try {
             SqlRowSet results = jdbcTemplate.queryForRowSet(sql, parkId);
             while (results.next()) {
